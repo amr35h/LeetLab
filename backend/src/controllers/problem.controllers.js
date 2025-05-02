36113f2051acd9764b9db296af42135835deecb4
Amr35h <amreshchaurasiya21@gmail.com>
2025-05-02 17:14:56 +0530

    Create Problem Controller Complete

---
+++ backend/src/controllers/problem.controllers.js
@@ -0,0 +1,89 @@
+import { db } from "../db/index.js";
+
+export const createProblem = async (req, res) => {
+  //going to call all the data from the request body
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testcases,
+    codeSnippets,
+    referenceSolutions,
+  } = req.body;
+
+  //going to check user role once again
+  if (req.user.role !== "ADMIN") {
+    return res
+      .status(403)
+      .json({ message: "You are not allowed to create a Problem" });
+  }
+
+  try {
+    for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
+      const languageId = getJudge0LanguageId(language);
+
+      if (!languageId) {
+        return res
+          .status(400)
+          .json({ message: `Language ${language} is not supported` });
+      }
+
+      const submissions = testcases.map(({ input, output }) => ({
+        source_code: solutionCode,
+        language_id: languageId,
+        stdin: input,
+        expected_output: output,
+      }));
+
+      const submissionResults = await submitBatch(submissions);
+
+      const tokens = submissionResults.map((res) => res.token);
+
+      const results = await pollBatchResults(tokens);
+
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+
+        if (result.status.id !== 3) {
+          return res.status(400).json({
+            message: `Testcase ${i + 1} failed for language ${language}`,
+          });
+        }
+      }
+
+      //save the problem in database
+      const newProblem = await db.problem.create({
+        data: {
+          title,
+          description,
+          difficulty,
+          tags,
+          examples,
+          constraints,
+          testcases,
+          codeSnippets,
+          referenceSolutions,
+          userId: req.user.id,
+        },
+      });
+
+      return res.status(201).json(newProblem);
+    }
+  } catch (error) {}
+
+  //loop through each refrenece solution for different languages
+  //
+};
+
+export const getAllProblems = async (req, res) => {};
+
+export const getProblemById = async (req, res) => {};
+
+export const updateProblem = async (req, res) => {};
+
+export const deleteProblem = async (req, res) => {};
+
+export const getAllProblemsSolvedByUser = async (req, res) => {};
