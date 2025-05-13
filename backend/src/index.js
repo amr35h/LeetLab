6aef4fca12979b0a66d251cf6e33423d4e8b59a9
Amr35h <amreshchaurasiya21@gmail.com>
2025-05-13 19:21:35 +0530

    Fronted Auth, Add Problem Done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import problemRoutes from "./routes/problem.routes.js";
@@ -11,6 +12,13 @@ import playlistRoutes from "./routes/playlist.routes.js";
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
